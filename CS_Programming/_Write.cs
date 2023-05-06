561bc01ec5119f22bce59ba1e4b0dc8fb780d19d
hickee032 <hickee32@gmail.com>
2023-05-06 12:00:00 +0900

    study

---
+++ CS_Programming/_Write.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Write
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Console.Write("write"); Console.Write("write");
+            Console.Write("write");
+Console.Write("write");
+        }
+    }
+}
---
+++ CS_Programming/_Write_WriteLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Write_WriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+            Console.WriteLine("WriteLine");
+            Console.Write("Write");
+        }
+    }
+}
