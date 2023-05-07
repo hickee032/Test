61d9407d6184aa1f166aa73fdeaadb16ecaa9a54
hickee032 <hickee32@gmail.com>
2023-05-07 10:00:00 +0900

    study

---
+++ CS_Programming/_dafaultdata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _dafaultdata
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine(4 % 3);
+            Console.WriteLine(-4 % 3);
+            Console.WriteLine(4 % -3);
+            Console.WriteLine(-4 % -3);
+        }
+    }
+}
---
+++ CS_Programming/_escapeChar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _escapeChar
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+        }
+    }
+}
