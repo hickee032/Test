38a0cebc7c34a8d5d16586771c31492d1d277fad
hickee032 <hickee32@gmail.com>
2023-05-08 10:00:00 +0900

    study

---
+++ CS_Programming/StringSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSelector
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello"[0]);
+            Console.WriteLine("Hello"[1]);
+            Console.WriteLine("Hello"[2]);
+            Console.WriteLine("Hello"[3]);
+            Console.WriteLine("Hello"[4]);
+        }
+    }
+}
