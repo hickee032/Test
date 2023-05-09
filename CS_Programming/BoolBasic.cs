3aaef474730986b1a7a4fc13a1e487af576f1ba9
hickee032 <hickee32@gmail.com>
2023-05-09 10:00:00 +0900

    study

---
+++ CS_Programming/BoolBasic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine(23 > 100);
+            Console.WriteLine(23 < 100);
+        }
+    }
+}
---
+++ CS_Programming/LogicalNot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalNot
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine(!true); 
+            Console.WriteLine(!false);
+            Console.WriteLine(!(20>100));
+            Console.WriteLine(!(20<100));
+        }
+    }
+}
