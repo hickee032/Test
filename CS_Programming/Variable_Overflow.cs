b5815fbe32a50250efdc326dd528df50fe2bf95e
hickee032 <hickee32@gmail.com>
2023-05-11 10:00:00 +0900

    study

---
+++ CS_Programming/Variable_Overflow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int a = 200;
+            int b = 50;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            //오버플로우
+
+            int c = 2147483640;
+            int d = 52273;
+
+            Console.WriteLine(c + d);
+
+            int e = 2000000000;
+            int f = 1000000000;
+            Console.WriteLine(e + f);
+
+        }
+    }
+}
---
+++ CS_Programming/unit_ulong.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_ulong
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            //음수를 사용하지 않을경우에 유용
+
+            uint unsignedInt = 4147483647;
+            ulong unsignedLong = 11223372036854775808;
+
+            Console.WriteLine(unsignedInt);
+            Console.WriteLine(unsignedLong);
+
+        }
+    }
+}
