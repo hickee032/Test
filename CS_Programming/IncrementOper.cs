5dd21f9fad84c09c9e318c9d5cbabc149cf5b34c
hickee032 <hickee32@gmail.com>
2023-05-15 10:00:00 +0900

    study

---
+++ CS_Programming/IncrementOper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementOper
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int num = 10;
+            num++;
+            Console.WriteLine(num);
+            num--;
+            Console.WriteLine(num);
+
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine(num);
+            Console.WriteLine(num++);
+            Console.WriteLine(num);
+            Console.WriteLine(num--);
+            Console.WriteLine(num);
+
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine(num);
+            Console.WriteLine(num); num += 1;
+            Console.WriteLine(num); num -= 1;
+            Console.WriteLine(num);
+
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine(num);
+            Console.WriteLine(num++);
+            Console.WriteLine(++num);
+            Console.WriteLine(num--);
+            Console.WriteLine(--num);
+
+
+        }
+    }
+}
