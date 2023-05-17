c281fcf7fd8c9ab6182ec15a26728de8f862b592
hickee032 <hickee32@gmail.com>
2023-05-17 10:00:00 +0900

    study

---
+++ CS_Programming/Cast.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            /*
+            long longNum = 2147483647L + 2147483647L;
+            int num = longNum;
+            Console.WriteLine(num);
+            
+
+            var a = (int)10.0;
+            var b = (float)10;
+            var c = (double)10;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            */
+
+            long longNum = 2147483647L + 2147483647L;
+            int num = (int)longNum;
+            Console.WriteLine(num);
+
+
+
+        }
+    }
+}
