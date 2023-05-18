4f3f523dfb6624872ba96955b30111b0afc99208
hickee032 <hickee32@gmail.com>
2023-05-18 10:00:00 +0900

    study

--- CS_Programming/Cast.cs
+++ CS_Programming/Cast.cs
@@ -9,11 +9,11 @@ namespace Input
     internal class Program
     {
         static void Main(string[] args) {
+
             /*
             long longNum = 2147483647L + 2147483647L;
             int num = longNum;
-            Console.WriteLine(num);
-            
+            Console.WriteLine(num);            
 
             var a = (int)10.0;
             var b = (float)10;
@@ -22,14 +22,27 @@ namespace Input
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
-            */
 
             long longNum = 2147483647L + 2147483647L;
             int num = (int)longNum;
             Console.WriteLine(num);
+            
+            long longNum = 522273;
+            int intNum = (int)longNum;
+            Console.WriteLine(intNum);
 
+            int intnum = 2147483647;
+            long inttolong = intnum;
+            Console.WriteLine(inttolong);
 
+            string numstr = "52273";
+            int intnum = (int)numstr;
+            Console.WriteLine(numstr);
+            */
 
+            Console.WriteLine(int.Parse("52"));
+            Console.WriteLine((52).ToString());
+            
         }
     }
 }
