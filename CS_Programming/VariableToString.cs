409b5cb26348b982e8c19287edcc4bd4a06a4929
hickee032 <hickee32@gmail.com>
2023-05-19 10:00:00 +0900

    study

---
+++ CS_Programming/VariableToString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleToString
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            /*
+            double num = 3.141592;
+            Console.WriteLine(num.ToString("0.0"));
+            Console.WriteLine(num.ToString("0.00"));
+            Console.WriteLine(num.ToString("0.000"));
+            */
+
+            int num = 52273;
+            string output = num + "";
+            Console.WriteLine(output);
+
+            char a = 'a';
+            string output2 = a + "";
+            Console.WriteLine(output2);
+
+            Console.WriteLine(bool.Parse("True"));
+            Console.WriteLine(bool.Parse("False"));
+
+            int input = int.MinValue;
+            Console.WriteLine(-input);
+        }
+    }
+}
